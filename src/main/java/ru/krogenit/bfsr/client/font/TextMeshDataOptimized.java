cd5edf15a542d9a2e8a86146bf8bbcc352e8fe37
Krogenit <tenkrg@gmail.com>
2020-10-03 01:45:24 +0300

    написан оптимизированный класс, совмещающий в себе позицию и текстурные координаты; помечен как Deprecated;

---
+++ src/main/java/ru/krogenit/bfsr/client/font/TextMeshDataOptimized.java
@@ -0,0 +1,19 @@
+package ru.krogenit.bfsr.client.font;
+
+@Deprecated
+public class TextMeshDataOptimized {
+
+    private final float[] vertexPositionsAndTextureCoords;
+
+    protected TextMeshDataOptimized(float[] vertexPositionsAndTextureCoords) {
+        this.vertexPositionsAndTextureCoords = vertexPositionsAndTextureCoords;
+    }
+
+    public float[] getVertexPositionsAndTextureCoords() {
+        return vertexPositionsAndTextureCoords;
+    }
+
+    public int getVertexCount() {
+        return vertexPositionsAndTextureCoords.length / 4;
+    }
+}
