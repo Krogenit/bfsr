4a4f81724b85c6bc34a811fa090184b55fe69e71
Krogenit <tenkrg@gmail.com>
2023-01-25 15:59:52 +0300

    исправлена позиция эелементов внутриигрового меню

--- core/src/main/java/net/bfsr/client/gui/ingame/GuiInGameMenu.java
+++ core/src/main/java/net/bfsr/client/gui/ingame/GuiInGameMenu.java
@@ -2,39 +2,27 @@ package net.bfsr.client.gui.ingame;
 
 import net.bfsr.client.gui.Gui;
 import net.bfsr.client.gui.GuiSettings;
-import net.bfsr.client.gui.GuiTextureObject;
+import net.bfsr.client.gui.TexturedGuiObject;
 import net.bfsr.client.gui.button.Button;
+import net.bfsr.client.language.Lang;
 import net.bfsr.client.render.Renderer;
-import net.bfsr.client.render.texture.TextureLoader;
 import net.bfsr.client.render.texture.TextureRegister;
 import net.bfsr.client.shader.BaseShader;
 import net.bfsr.client.shader.ShaderProgram;
 import net.bfsr.core.Core;
-import net.bfsr.entity.TextureObject;
 import net.bfsr.math.Transformation;
 import org.lwjgl.glfw.GLFW;
 
 public class GuiInGameMenu extends Gui {
-    private final TextureObject logoBFSR;
     private boolean wantCloseGui;
-
-    public GuiInGameMenu() {
-        logoBFSR = new GuiTextureObject(TextureLoader.getTexture(TextureRegister.guiLogoBFSR));
-    }
 
     @Override
     protected void initElements() {
-        Button button = new Button(center.x, center.y - 30, "gui.ingamemenu.backtogame", () -> Core.getCore().setCurrentGui(null));
-        button.setTextColor(1, 1, 1, 1);
-        registerGuiObject(button);
-        button = new Button(center.x, center.y + 30, "gui.ingamemenu.settings", () -> Core.getCore().setCurrentGui(new GuiSettings(this)));
-        button.setTextColor(1, 1, 1, 1);
-        registerGuiObject(button);
-        button = new Button(center.x, center.y + 180, "gui.ingamemenu.tomainmenu", () -> Core.getCore().quitToMainMenu());
-        button.setTextColor(1, 1, 1, 1);
-        registerGuiObject(button);
-        logoBFSR.setPosition(center.x, center.y - 200);
-        logoBFSR.setScale(256, 256);
+        int x = -150;
+        registerGuiObject(new Button(Lang.getString("gui.ingamemenu.backtogame"), () -> Core.getCore().setCurrentGui(null)).atCenter(x, -30));
+        registerGuiObject(new Button(Lang.getString("gui.ingamemenu.settings"), () -> Core.getCore().setCurrentGui(new GuiSettings(this))).atCenter(x, 30));
+        registerGuiObject(new Button(Lang.getString("gui.ingamemenu.tomainmenu"), () -> Core.getCore().quitToMainMenu()).atCenter(x, 180));
+        registerGuiObject(new TexturedGuiObject(TextureRegister.guiLogoBFSR).atCenter(-128, -328).setSize(256, 256));
     }
 
     @Override
@@ -63,7 +51,6 @@ public class GuiInGameMenu extends Gui {
         shader.disableTexture();
         Renderer.centeredQuad.renderIndexed();
 
-        logoBFSR.render(shader);
         super.render(shader);
     }
 }
