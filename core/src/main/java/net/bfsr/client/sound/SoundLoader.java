2ce2427cfcf75178cbd658e50df94d466eeb439d
Krogenit <tenkrg@gmail.com>
2023-02-13 18:56:39 +0300

    code cleanup

--- core/src/main/java/net/bfsr/client/sound/SoundLoader.java
+++ core/src/main/java/net/bfsr/client/sound/SoundLoader.java
@@ -1,26 +1,29 @@
 package net.bfsr.client.sound;
 
 import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.EnumMap;
 
 @Log4j2
 final class SoundLoader {
     private static final EnumMap<SoundRegistry, SoundBuffer> LOADED_SOUNDS = new EnumMap<>(SoundRegistry.class);
 
-    private static void loadSound(SoundRegistry sound) {
+    @Nullable
+    private static SoundBuffer loadSound(SoundRegistry sound) {
         try {
             SoundManager manager = SoundManager.getInstance();
-            SoundBuffer buffBack = new SoundBuffer(sound.getPath());
-            manager.addSoundBuffer(buffBack);
-            LOADED_SOUNDS.put(sound, buffBack);
-        } catch (Exception e) {
+            SoundBuffer soundBuffer = new SoundBuffer(sound.getPath());
+            manager.addSoundBuffer(soundBuffer);
+            return soundBuffer;
+        } catch (IOException e) {
             log.error("Can't load sound {}", sound.getPath(), e);
+            return null;
         }
     }
 
     static int getBufferId(SoundRegistry soundName) {
-        if (!LOADED_SOUNDS.containsKey(soundName)) loadSound(soundName);
-        return LOADED_SOUNDS.get(soundName).getBufferId();
+        return LOADED_SOUNDS.computeIfAbsent(soundName, soundRegistry -> loadSound(soundName)).getBufferId();
     }
 }
