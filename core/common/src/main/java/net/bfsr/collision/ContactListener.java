202e58663219475a45c63acd1d283a49b8087b06
Krogenit <tenkrg@gmail.com>
2023-03-17 21:56:58 +0300

    using postSolve method instead of begin in ContactListener

--- core/common/src/main/java/net/bfsr/collision/ContactListener.java
+++ core/common/src/main/java/net/bfsr/collision/ContactListener.java
@@ -2,24 +2,26 @@ package net.bfsr.collision;
 
 import net.bfsr.entity.GameObject;
 import org.dyn4j.dynamics.Body;
-import org.dyn4j.dynamics.contact.Contact;
+import org.dyn4j.dynamics.contact.SolvedContact;
+import org.dyn4j.geometry.Vector2;
 import org.dyn4j.world.ContactCollisionData;
 import org.dyn4j.world.listener.ContactListenerAdapter;
 
 public class ContactListener extends ContactListenerAdapter<Body> {
     @Override
-    public void begin(ContactCollisionData<Body> collision, Contact contact) {
+    public void postSolve(ContactCollisionData<Body> collision, SolvedContact contact) {
+        Vector2 normal = collision.getContactConstraint().getNormal();
         Body body1 = collision.getBody1();
         Body body2 = collision.getBody2();
 
         Object userData = body1.getUserData();
         if (userData instanceof GameObject gameObject) {
-            gameObject.checkCollision(contact, collision.getContactConstraint().getNormal(), body2);
+            gameObject.checkCollision(contact, normal, body2);
         }
 
         userData = body2.getUserData();
         if (userData instanceof GameObject gameObject) {
-            gameObject.checkCollision(contact, collision.getContactConstraint().getNormal(), body1);
+            gameObject.checkCollision(contact, normal, body1);
         }
     }
 }
