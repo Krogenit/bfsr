15a97f8266430455c074fae90789ec000d54a1b3
Krogenit <tenkrg@gmail.com>
2023-04-18 04:55:14 +0300

    code cleanup

--- core/client/src/main/java/net/bfsr/client/renderer/debug/OpenGLDebugUtils.java
+++ core/client/src/main/java/net/bfsr/client/renderer/debug/OpenGLDebugUtils.java
@@ -3,59 +3,39 @@ package net.bfsr.client.renderer.debug;
 import static org.lwjgl.opengl.GL43C.*;
 import static org.lwjgl.system.APIUtil.apiUnknownToken;
 
-public class OpenGLDebugUtils {
+public final class OpenGLDebugUtils {
     public static String getDebugSeverity(int severity) {
-        switch (severity) {
-            case GL_DEBUG_SEVERITY_HIGH:
-                return "HIGH";
-            case GL_DEBUG_SEVERITY_MEDIUM:
-                return "MEDIUM";
-            case GL_DEBUG_SEVERITY_LOW:
-                return "LOW";
-            case GL_DEBUG_SEVERITY_NOTIFICATION:
-                return "NOTIFICATION";
-            default:
-                return apiUnknownToken(severity);
-        }
+        return switch (severity) {
+            case GL_DEBUG_SEVERITY_HIGH -> "HIGH";
+            case GL_DEBUG_SEVERITY_MEDIUM -> "MEDIUM";
+            case GL_DEBUG_SEVERITY_LOW -> "LOW";
+            case GL_DEBUG_SEVERITY_NOTIFICATION -> "NOTIFICATION";
+            default -> apiUnknownToken(severity);
+        };
     }
 
     public static String getDebugSource(int source) {
-        switch (source) {
-            case GL_DEBUG_SOURCE_API:
-                return "API";
-            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-                return "WINDOW SYSTEM";
-            case GL_DEBUG_SOURCE_SHADER_COMPILER:
-                return "SHADER COMPILER";
-            case GL_DEBUG_SOURCE_THIRD_PARTY:
-                return "THIRD PARTY";
-            case GL_DEBUG_SOURCE_APPLICATION:
-                return "APPLICATION";
-            case GL_DEBUG_SOURCE_OTHER:
-                return "OTHER";
-            default:
-                return apiUnknownToken(source);
-        }
+        return switch (source) {
+            case GL_DEBUG_SOURCE_API -> "API";
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM -> "WINDOW SYSTEM";
+            case GL_DEBUG_SOURCE_SHADER_COMPILER -> "SHADER COMPILER";
+            case GL_DEBUG_SOURCE_THIRD_PARTY -> "THIRD PARTY";
+            case GL_DEBUG_SOURCE_APPLICATION -> "APPLICATION";
+            case GL_DEBUG_SOURCE_OTHER -> "OTHER";
+            default -> apiUnknownToken(source);
+        };
     }
 
     public static String getDebugType(int type) {
-        switch (type) {
-            case GL_DEBUG_TYPE_ERROR:
-                return "ERROR";
-            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-                return "DEPRECATED BEHAVIOR";
-            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-                return "UNDEFINED BEHAVIOR";
-            case GL_DEBUG_TYPE_PORTABILITY:
-                return "PORTABILITY";
-            case GL_DEBUG_TYPE_PERFORMANCE:
-                return "PERFORMANCE";
-            case GL_DEBUG_TYPE_OTHER:
-                return "OTHER";
-            case GL_DEBUG_TYPE_MARKER:
-                return "MARKER";
-            default:
-                return apiUnknownToken(type);
-        }
+        return switch (type) {
+            case GL_DEBUG_TYPE_ERROR -> "ERROR";
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR -> "DEPRECATED BEHAVIOR";
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR -> "UNDEFINED BEHAVIOR";
+            case GL_DEBUG_TYPE_PORTABILITY -> "PORTABILITY";
+            case GL_DEBUG_TYPE_PERFORMANCE -> "PERFORMANCE";
+            case GL_DEBUG_TYPE_OTHER -> "OTHER";
+            case GL_DEBUG_TYPE_MARKER -> "MARKER";
+            default -> apiUnknownToken(type);
+        };
     }
 }
