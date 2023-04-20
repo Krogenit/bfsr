e787bb2b8642840be5103a116d08fd85e410e43a
Krogenit <tenkrg@gmail.com>
2023-04-20 12:43:52 +0300

    change collection name

--- core/server/src/main/java/net/bfsr/server/dto/PlayerModel.java
+++ core/server/src/main/java/net/bfsr/server/dto/PlayerModel.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "players")
+@Document(collection = "player")
 public record PlayerModel(
         @Id
         ObjectId id,
