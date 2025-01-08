2955246e14c8947b93ab652095f23a996ba82b9f
Krogenit <tenkrg@gmail.com>
2025-01-08 20:23:20 +0300

    Version 0.1.6

--- game-logic/client/src/main/java/net/bfsr/client/Client.java
+++ game-logic/client/src/main/java/net/bfsr/client/Client.java
@@ -48,7 +48,7 @@ import java.net.InetAddress;
 @Log4j2
 @Getter
 public class Client extends ClientGameLogic {
-    public static final String GAME_VERSION = "Dev 0.1.5";
+    public static final String GAME_VERSION = "Dev 0.1.6";
     private static Client instance;
 
     private final ConfigConverterManager configConverterManager = new ConfigConverterManager();
