e5d061359b183afd5c7e660354df22152f856c9a
Krogenit <tenkrg@gmail.com>
2025-01-21 11:42:22 +0300

    Add root path system property for PathHelper

--- engine/common/src/main/java/net/bfsr/engine/util/PathHelper.java
+++ engine/common/src/main/java/net/bfsr/engine/util/PathHelper.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public final class PathHelper {
-    public static final Path CLIENT_CONTENT = Path.of("../../assets/client");
-    private static final Path CONTENT = Path.of("../../assets/common");
+    private static final String ROOT_PATH = System.getProperty("assets.path", ".");
+
+    public static final Path CLIENT_CONTENT = Path.of(ROOT_PATH + "assets/client");
+    private static final Path CONTENT = Path.of(ROOT_PATH + "assets/common");
     public static final Path CONFIG = CONTENT.resolve("config");
     public static final Path SHADER = CLIENT_CONTENT.resolve("shader");
     public static final Path FONT = CLIENT_CONTENT.resolve("font");
